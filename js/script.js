098eb305ee538de7efa80ba576d4a7d357c6c155
ahhangok <62680930+ahhangok@users.noreply.github.com>
2020-10-17 10:00:28 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,18 +1,6 @@
 function scrollHandler(){
   var windowBottom = $(window).scrollTop() +$(window).height();
   
-  //모든 플레이 리스트를 하나씩 살펴보고
-  //그 플레이 리스트의 중간 지점의 좌표가 windowBottom보다 작으면
-  //그 플레이 리스트를 보이게 해라
- // var playlists = $('.playlist');
- // for(var i=0; i<playlists.length; i++){
-    //var playlist = $(playlists[i]);
-   // var playlistHalf = playlist.position().top + playlist.outerHeight() / 2;
-    
-   // if(playlistHalf < windowBottom){
-     // playlist.animate({'opacity': '1'},1500);
-   // }    
- // }
   $('.playlist').each(function(){
    var playlist = $(this);
     
@@ -34,10 +22,8 @@ function scrollHandler(){
 
 $(window).on('scroll',scrollHandler);
 
-//처음 로딩될때 호출
 scrollHandler();
 
-//맨위로 올라가기
 $('.to-top-btn').on('click',function(){
   $('html,body').animate({scrollTop: 0},1000);
 });
